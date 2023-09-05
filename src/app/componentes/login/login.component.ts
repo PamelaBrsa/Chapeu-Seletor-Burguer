207099dd461dd02e790b9cc6edb4bcd1ed09c28d
PamelaBrsa <pamela.barbosa16@yahoo.com.br>
2023-09-05 16:58:59 -0300

    requisiçãoHTTP

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { LoginService } from 'src/app/services/login/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  nome: string = '';
+  senha: string = '';
+  loginError: boolean = false;
 
+  constructor(private authService: LoginService, private router: Router) { }
+
+  //   async logar (){
+  //     try {
+  //       this.loginError = false;
+  //       const usuarioLogado = await this.authService.login(this.nome, this.senha);
+  //       console.log(usuarioLogado)
+  //       if(usuarioLogado){
+  //         const usuario = localStorage.getItem('usuario');
+  //         switch(usuario){
+  //           case 'admin':
+  //             this.router.navigate(['/menu'])
+  //             break;
+
+  //         }
+  //       } else {
+  //         this.loginError = true;
+  //         throw new Error ('Login Inválido');
+  //       }
+  //     } catch (error) {
+  //       console.error ('Error no login:', error);
+  //       this.loginError = true;
+  //     }
+  //   }
+  // }
+
+
+
+  logar(): void {
+    this.authService.login(this.nome, this.senha).subscribe(
+      (response) => {
+        console.log(response)
+        const token = response.accessToken;
+        const role = response.user.role
+
+        this.authService.saveRole(role)
+        console.log(role)
+        this.authService.saveToken(token)
+
+        this.router.navigate(['/home'])
+
+      }
+    )
+  }
 }
+  // (error) = {
+  //   console.error('Erro no login', error)
+  //   this.loginError = true;
+  //   this.errorMessage = 'Credenciais inválidas. Verifique seu e-mail e senha.';
+  // }
--- src/app/componentes/login/login.module.ts
+++ src/app/componentes/login/login.module.ts
@@ -1,21 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
-import { FormLoginComponent } from './form-login/form-login.component';
-import { CardUserComponent } from './card-user/card-user.component';
 
 
 @NgModule({
   declarations: [
     LoginComponent,
-    FormLoginComponent,
-    CardUserComponent
+
   ],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    FormsModule  
   ],
   exports: [
     LoginComponent
